3d83d8dbaa36d12d8c63e42d7bf8c63e694b1077
Francois Blackburn <fblackburn@wazo.community>
2017-04-21 13:53:55 -0400

    service: fix styling issues

--- cdr/service.py
+++ cdr/service.py
@@ -4,7 +4,7 @@
 
 import logging
 
-from wazo_admin_ui.helpers.call_logd import calllogd
+from wazo_admin_ui.helpers.call_logd import call_logd
 
 logger = logging.getLogger(__name__)
 
@@ -14,7 +14,7 @@ class CdrService(object):
     resource_calllogd = 'cdr'
 
     def list(self, limit=None, order=None, direction=None, offset=None, search=None, **kwargs):
-        resource_client = getattr(calllogd, self.resource_calllogd)
+        resource_client = getattr(call_logd, self.resource_calllogd)
         return resource_client.list(search=search,
                                     order=order,
                                     limit=limit,
