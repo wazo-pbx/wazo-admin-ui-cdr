b858a80046d2920fcf9f7fc89200dec6cb0d61b6
Cedric Abunar <cabunar@wazo.io>
2018-02-23 11:52:33 -0500

    update translations

--- wazo_plugind_admin_ui_cdr_official/view.py
+++ wazo_plugind_admin_ui_cdr_official/view.py
@@ -1,16 +1,16 @@
-# Copyright 2017 The Wazo Authors  (see the AUTHORS file)
+# Copyright 2017-2018 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0+
 
+from flask_babel import lazy_gettext as l_
 from flask_menu.classy import classy_menu_item
 
 from wazo_admin_ui.helpers.classful import BaseView, IndexAjaxViewMixin
 
 
 class CdrView(IndexAjaxViewMixin, BaseView):
-
     form = object
     resource = 'cdr'
 
-    @classy_menu_item('.cdrs', 'CDR', order=4, icon="newspaper-o")
+    @classy_menu_item('.cdrs', l_('CDR'), order=4, icon="newspaper-o")
     def index(self):
         return super(CdrView, self).index()
